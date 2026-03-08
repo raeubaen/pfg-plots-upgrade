243514cb6a575eabdb065c692b6c628e895983bd
ruben <r@g.it>
2026-03-08 17:55:49 +0100

    2025 -> 6 in some file

--- get_runlist_for_plots_multiple_weeks.sh
+++ get_runlist_for_plots_multiple_weeks.sh
@@ -2,7 +2,7 @@ echo "weeks from $1 to $2" >&2
 
 files=""
 for w in $(seq $1 $2); do
-  for f in $(ls -d /eos/project/c/cms-ecalpfg2/www/PFGshifts/PERFORMANCE2025/week$w/*); do
+  for f in $(ls -d /eos/project/c/cms-ecalpfg2/www/PFGshifts/PERFORMANCE2026/week$(printf "%02d\n" "$w")/*); do
       files=$(echo $files $(ls -1 $f/runlist_for_report.txt 2>&1 | grep -v "cannot"));
   done
 done
@@ -10,3 +10,4 @@ done
 
 echo "run,dataset"
 cat $files | grep "collision"  | awk '$6 > 100' | awk '{print $4",/Global/Online/ALL/"}' | grep -v run
+#cat $files |  awk '{print $4",/Global/Online/ALL/"}' | grep -v run
